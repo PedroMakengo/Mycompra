24955a17502b88c0cdc4e68e722e2682bb86360d
PedroMakengo <61626420+PedroMakengo@users.noreply.github.com>
2021-10-20 04:26:56 -0300

    Criando as rotas

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,20 @@
+const { render } = require("ejs");
 const express = require("express");
 const routes = express.Router();
 
 const views = __dirname + "/views/";
-// const viewsDash = __dirname + "/views/dashboard";
+const viewsDash = __dirname + "/views/dashboard/";
 
+// Router site
 routes.get("/", (req, res) => res.render(views + "index"));
-
 routes.get("/create", (req, res) => {
   res.render(views + "create");
 });
-
 routes.get("/login", (req, res) => {
   res.render(views + "login");
 });
+
+// Router dashboard
+routes.get("/dashboard", (req, res) => res.render(viewsDash + "index"));
 
 module.exports = routes;
